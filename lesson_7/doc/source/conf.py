6e3cbeaa56090489d6b7ea947dd7b527a184252a
bearlexxx <alexey.d84@gmail.com>
2022-08-28 11:31:33 +0300

    lesson_7

---
+++ lesson_7/doc/source/conf.py
@@ -0,0 +1,44 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+path = os.path.dirname(os.path.dirname(os.path.abspath('.')))
+sys.path.insert(0, path)
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'GB DB and PyQT'
+copyright = '2022, GB User'
+author = 'GB User'
+release = '001'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
